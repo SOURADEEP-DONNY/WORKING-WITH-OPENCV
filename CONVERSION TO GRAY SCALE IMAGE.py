1712e48d25411f40a598f15d405a7f0ad944f587
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-15 22:30:34 +0530

    Add files via upload

--- CONVERSION TO GRAY SCALE IMAGE.py
+++ CONVERSION TO GRAY SCALE IMAGE.py
@@ -1,12 +1,7 @@
 import cv2
 #READING AN IMAGE AND SHOWING AN IMAGE.
-img=cv2.imread('lady.tiff')
-cv2.imshow('original',img)
+img=cv2.imread('lady.tiff',0)
+cv2.imshow('grayscale image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#CONVERTING THE IMAGE TO GRAYSCALE IMAGE FROM COLOUR MAGE
-img1=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow('grayimage',img1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
